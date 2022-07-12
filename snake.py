722757cf0cf84ebb8f577b3a37b21ce25a128353
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2022-07-13 00:27:55 +0300

    snake.py changes

--- snake.py
+++ snake.py
@@ -1,5 +1,5 @@
 import curses
-
+# from random import randind
 # setup window
 
 curses.initscr()
@@ -22,14 +22,55 @@ ESC = 27
 key = curses.KEY_RIGHT
 
 while key != ESC:
-    win.addch(0, 2, 'Score ')
+    win.addch(0, 2, 'Score ' + str(score) + ' ')
+    win.timeout(150 - (len(snake)) // len(snake)//10 % 120)    # increase speed
 
+    prev_key = key
     event = win.getch()
+    key = event if event != -1 else prev_key
 
-    for c in snake:
-        win.addch(c[0], c[1], '*')
+    if key not in [curses.KEY_LEFT, curses.KEY_RIGHT, curses.KEY_UP, curses.KEY_DOWN, ESC]:
+        key = prev_key
 
-    win.addch(food[0], food[1], '#')
+    # calculate the next coordinates
+    y = snake[0][0]
+    x = snake[0][1]
+    if key == curses.KEY_DOWN:
+        y += 1
+    if key == curses.KEY_UP:
+        y -= 1
+    if key == curses.KEY_LEFT:
+        y -= 1
+    if key == curses.KEY_RIGHT:
+        y += 1
+
+    snake.insert(0, (y, x))  # append 0(n)
+
+    # check if we hit the border
+    if y == 0: break
+    if y == 19: break
+    if x == 0: break
+    if x == 59: break
+
+    # if snake runs over itself
+    if snake[0] in snake[1:]: break
+
+    if snake[0] == food:
+        # eat the food
+        score += 1
+        food = ()
+        while food == ():
+            # food = (randint(1,18), randint(1,58))
+            food = (8, 8)
+            if food in snake:
+                food = ()
+        win.addch(food[0], food[1], '#')
+    else:
+        # move snake
+        last = snake.pop()
+        win.addch(last[0], last[1], '')
+
+    win.addch(snake[0][0], snake[0][1], '*')
 
 
 curses.endwin()
